6f97f9ed70a8b35d2fc5c3db404072a316fc7b2f
priyajais1994 <kumaripriyanka271439@gmail.com>
2023-01-19 17:19:15 +0530

    arrey problem

---
+++ calculator app1.js
@@ -0,0 +1,28 @@
+let a= parseInt(prompt("enter 1st no"));
+let b= parseInt(prompt("enter 2nd no"));
+        function Add()
+        {
+            document.write(a+b);
+        }
+        function subtract()
+        {
+            document.write(a-b);
+        }
+        function multiply()
+        {
+            document.write(a*b);
+        }
+        function divide()
+        {
+            document.write(a/b);
+        }
+        function sqrt()
+        {
+            document.write(Math.sqrt(a));
+            document.write(Math.sqrt(b));
+        }
+        function power()
+        {
+            document.write(Math.pow(a,2));
+            document.write(Math.pow(b,2));
+        }
